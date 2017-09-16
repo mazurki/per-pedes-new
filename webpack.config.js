c14bd8c186b11839b2c850a0f1664405ea2d59ea
MBorecki <borecki2@gmail.com>
2017-09-16 20:07:40 +0200

    dalsze porzadki

--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,63 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const extractSCSS = new ExtractTextPlugin("css/styles.css");
+
+
+const SOURCE_DIR = path.join(__dirname, '/src');
+const DIST_DIR = path.join(__dirname, '/dist');
+const MAIN_JS_FILE = path.join(SOURCE_DIR, 'scripts/app.js');
 
 let plugins = [];
 
 plugins.push(new HtmlWebpackPlugin({
     filename: 'index.html',
-    template: 'pages/index.html'
+    template: path.join(SOURCE_DIR, 'index.html')
 }));
 
-plugins.push(new ExtractTextPlugin("styles.css"));
-plugins.push(new CleanWebpackPlugin(__dirname + '/dist'));
+plugins.push(extractSCSS);
+plugins.push(new CleanWebpackPlugin(DIST_DIR));
 
 module.exports = {
     output: {
-        path: __dirname + '/dist',
-        filename: 'main.js'
+        path: DIST_DIR,
+        filename: 'scripts/main.js'
     },
-    entry: __dirname + '/assets/scripts/app.js',
+    entry: MAIN_JS_FILE,
 
     module: {
         rules: [
             {
                 test: /\.s?css$/,
-                use: ExtractTextPlugin.extract([
+                use: extractSCSS.extract([
                     {
                         loader: "css-loader"
                     },{
                         loader: 'sass-loader'
                     }
                 ])
-            },{
+            },
+            {
                 test: /\.(eot|svg|ttf|woff|woff2)$/,
+                exclude: [/img/],
                 use: {
                     loader: 'file-loader',
                     options: {
-                        name: '[path][name].[ext]'
+                        name: '[name].[ext]?[hash]',
+                        outputPath: 'fonts/'
                     }
                 }
-            },{
-                test: /\.(jpg|png|gif)$/,
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                exclude: [/fonts/],
                 use: {
                     loader: 'file-loader',
                     options: {
-                        name: '[path][name].[ext]?[hash]'
+                        name: '[name].[ext]?[hash]',
+                        useRelativePath: true
                     }
                 }
             }
